b0f4dbc0a09bf4e62d5d6939f383121f0a282470
Vadim <vtimofeev223@gmail.com>
2021-06-29 13:12:55 +0300

    Maked changes regarding element position

--- vue/components/FacebookAdminSettings.vue
+++ vue/components/FacebookAdminSettings.vue
@@ -7,7 +7,7 @@
       <q-card flat bordered class="card-edit-settings">
         <q-card-section>
           <div class="row q-mb-md">
-            <q-checkbox dense v-model="enableFacebook" color="teal">
+            <q-checkbox dense v-model="enableFacebook">
               <q-item-label v-t="'FACEBOOK.ENABLE_MODULE'"/>
             </q-checkbox>
           </div>
@@ -29,7 +29,7 @@
             </q-item-label>
           </div>
           <div class="row q-mt-md">
-            <q-checkbox dense v-model="auth" color="teal">
+            <q-checkbox dense v-model="auth">
               <q-item-label v-t="'FACEBOOKAUTHWEBCLIENT.SCOPE_AUTH'"/>
             </q-checkbox>
           </div>
